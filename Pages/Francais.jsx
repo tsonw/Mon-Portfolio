b622a596e45c53704e290060bcb9f806943ff5f5
thoang3 <thai-son.hoang@universite-paris-saclay.fr>
2025-07-08 22:28:15 +0200

    Updates

---
+++ Pages/Francais.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../Styles/Home/Home.css';
+import NavBar from '../Components/navBar'
+import Home from "./Home";
+
+export default function Francais() {
+
+    return (
+        <>
+            <NavBar />
+            <Home />
+        </>
+    );
+}
